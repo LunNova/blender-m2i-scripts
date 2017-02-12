ca65652be148a644092e3c946d58213ff7a4f47f
suncurio <suncurio@gmail.com>
2017-02-12 21:19:52 +0300

    improve pose tools. now check armature is selected, and do all actions respectedly to it

--- WowTools/wow_pose_tools.py
+++ WowTools/wow_pose_tools.py
@@ -4,87 +4,70 @@
 import bpy
 import re
 
-#******************************
-#---===GUI===
-#******************************
-class OBJECT_PT_WoW_Pose(bpy.types.Panel):
-	
-	bl_label = 'WoW Pose Tools'
-	#bl_space_type = 'PROPERTIES'
-	#bl_region_type = 'WINDOW'
-	#bl_context = 'scene'
-	bl_idname = 'WoWPoseTools'
-	bl_space_type = 'VIEW_3D'
-	bl_region_type = 'TOOLS'
-	bl_category = 'WoW'
-	
-	def draw(self, context):
-		self.layout.operator('scene.wow_create_modifiers', text='Create armature mods');
-		self.layout.operator('scene.wow_apply_modifiers', text='Apply armature mods');
-		self.layout.operator('scene.wow_apply_pose', text='Apply pose');
+class OBJECT_OP_Create_Modifiers(bpy.types.Operator):
+	bl_idname = 'scene.wow_create_modifiers'
+	bl_label = 'Create armature modifiers'
+	bl_description = 'Create armature modifiers on all meshes, which are children to selected armature.'
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None and context.active_object.type == 'ARMATURE'
+
+	def execute(self, context):
+		armature = context.active_object
+
+		for ob in bpy.context.scene.objects:
+			if ob.type != 'MESH' or ob.parent != armature:
+				continue
+
+			found=False
+			#chech mesh already has armature modifier
+			for mod in ob.modifiers:
+				if mod.type == 'ARMATURE' and mod.object == armature:
+					found=True
+					break
+			if found == False:
+				mod = ob.modifiers.new('Armature', 'ARMATURE')
+				mod.object = armature
+		return {'FINISHED'}
 
 class OBJECT_OP_Apply_Modifiers(bpy.types.Operator):
 	bl_idname = 'scene.wow_apply_modifiers'
 	bl_label = 'Apply armature modifiers'
-	bl_description = 'Apply armature modifiers to all meshes.'
-	
+	bl_description = 'Apply armature modifiers to all meshes, which are children to selected armature.'
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None and context.active_object.type == 'ARMATURE'
+
 	def execute(self, context):
+		if bpy.context.active_object is None or bpy.context.active_object.type != 'ARMATURE':
+			self.report({'ERROR'}, "Armature must be selected")
+			return {'FINISHED'}
+
+		armature = bpy.context.active_object;
 		#bpy.ops.object.select_all(action = 'DESELECT')
 		for ob in bpy.context.scene.objects:
 			if ob.type == 'MESH':
 				bpy.context.scene.objects.active = ob
 				for mod in ob.modifiers:
-					if mod.type == 'ARMATURE':
+					if mod.type == 'ARMATURE' and mod.object == armature:
 						bpy.ops.object.modifier_apply(apply_as='DATA', modifier=mod.name)
 		return {'FINISHED'}
-		
-class OBJECT_OP_Create_Modifiers(bpy.types.Operator):
-	bl_idname = 'scene.wow_create_modifiers'
-	bl_label = 'Create armature modifiers'
-	bl_description = 'Create armature modifiers on all meshes, which does not have any. Mesh\'s parent should be source armature.'
-	
-	def execute(self, context):
-		#validate
-		for ob in bpy.context.scene.objects:
-			if ob.type == 'MESH':
-				bpy.context.scene.objects.active = ob
-				found=False
-				#chech mesh already has armature modifier
-				for mod in ob.modifiers:
-					if mod.type == 'ARMATURE':
-						found=True
-						break
-				if found == False:
-					if ob.parent is None or ob.parent.type != 'ARMATURE':
-						self.report({'ERROR'}, "Mesh '%s' does not have parent or parent is not armature" % ob.name)
-						return {'FINISHED'}
 
-		#bpy.ops.object.select_all(action = 'DESELECT')
-		#apply
-		for ob in bpy.context.scene.objects:
-			if ob.type == 'MESH':
-				bpy.context.scene.objects.active = ob
-				found=False
-				#chech mesh already has armature modifier
-				for mod in ob.modifiers:
-					if mod.type == 'ARMATURE':
-						found=True
-						break
-				if found == False:
-					mod = ob.modifiers.new('Armature', 'ARMATURE')
-					if ob.parent is not None and ob.parent.type == 'ARMATURE':
-						mod.object = ob.parent
-		return {'FINISHED'}
-		
 class OBJECT_OP_Apply_Pose(bpy.types.Operator):
 	bl_idname = 'scene.wow_apply_pose'
-	bl_label = 'Apply pose'
-	bl_description = 'Apply current pose as rest pose. Original Blender function is bugged - it does not respect attachments.'
+	bl_label = 'Apply armature pose'
+	bl_description = 'Apply currently selected armature pose as rest pose. Original Blender function is bugged - it does not respect attachments.'
+
+	@classmethod
+	def poll(cls, context):
+		return context.active_object is not None and context.active_object.type == 'ARMATURE'
 
 	def execute(self, context):
-		if bpy.context.active_object is None or bpy.context.active_object.mode != 'POSE':
-			self.report({'ERROR'}, "Must be in Pose mode")
-			return {'FINISHED'}
+		if bpy.context.active_object.mode != 'POSE':
+			bpy.ops.object.mode_set(mode = 'POSE') 
+
 		BoneByAttach = dict()
 		ParentByAttach = dict()
 		# detach attachments
@@ -114,7 +97,7 @@ bpy.types.WindowManager.iWowTools_WeightThreshold = bpy.props.FloatProperty(
 	min=0.0,
 	max=1.0,
 	soft_max=1.0)
-	
+
 class DATA_PT_wowtools_vertex_props(bpy.types.Panel):
 	bl_label = "Weights cleanup"
 	bl_idname = "wowtools.vertex_ops"
@@ -146,13 +129,13 @@ class DATA_OT_wowtools_cleanup_weights(bpy.types.Operator):
 
 	bl_idname = "wowtools.cleanup_weights"
 	bl_label = "Wow Tools Bone Weight Cleanup"
+	bl_description = "Removes weights from vertices that are below threshold and remove unused vertex groups"
 
 	@classmethod
 	def poll(cls, context):
 		return context.active_object is not None and context.active_object.type == 'MESH'
 
 	def execute(self, context):
-
 		oTargetObject = context.active_object
 
 		usedVertexGroups = dict()
@@ -171,10 +154,24 @@ class DATA_OT_wowtools_cleanup_weights(bpy.types.Operator):
 			if VertexGroup.index not in usedVertexGroups:
 				groupsToRemove.append(VertexGroup)
 
-
 		for group in groupsToRemove:
 			oTargetObject.vertex_groups.remove(group)
 
 
 		self.report({'INFO'}, "Vertex weights cleaned up")
 		return {'FINISHED'}
+
+#******************************
+#---===GUI===
+#******************************
+class OBJECT_PT_WoW_Pose(bpy.types.Panel):
+	bl_label = 'WoW Pose Tools'
+	bl_idname = 'WoWPoseTools'
+	bl_space_type = 'VIEW_3D'
+	bl_region_type = 'TOOLS'
+	bl_category = 'WoW'
+
+	def draw(self, context):
+		self.layout.operator('scene.wow_create_modifiers', text='Create armature modifiers');
+		self.layout.operator('scene.wow_apply_modifiers', text='Apply armature modifiers');
+		self.layout.operator('scene.wow_apply_pose', text='Apply armature pose');
